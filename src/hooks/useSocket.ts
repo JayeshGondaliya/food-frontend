81d1c22cdf17f7e411a0c20a0428f360c4b3b684
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2026-02-25 21:49:03 +0530

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL || "https://food-backend-ak09.onrender.com";
 
 export const useSocket = (event: string, callback: (data: any) => void) => {
   const socketRef = useRef<Socket | null>(null);
