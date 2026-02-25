954d31bc805ada71a19e7251bffe894170c9282a
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2026-02-25 22:47:12 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://food-backend-ak09.onrender.com';
+const API_URL = 'https://food-backend-ak09.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
